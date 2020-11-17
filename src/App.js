7aa206a4b7a1cbf028dd0b8ec17732b0ac2211c3
seyeon <tltptltptltp@gmail.com>
2020-11-17 23:43:02 +0900

    rlqhstptxld

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
+import React, {Component} from "react";
+import Router from "Components/Router";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <><Router/></>;
+  }
 }
 
 export default App;
